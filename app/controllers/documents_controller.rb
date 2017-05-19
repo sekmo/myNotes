da728d2e8e848c938ca3cfda61713a9e2c6bbe0b
Francesco Mari <francescojjmari@gmail.com>
2017-05-19 18:43:50 +0200

    Added DocumentsController and routes

---
+++ app/controllers/documents_controller.rb
@@ -0,0 +1,29 @@
+class DocumentsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def find_document
+  end
+  def document_params
+  end
+end
