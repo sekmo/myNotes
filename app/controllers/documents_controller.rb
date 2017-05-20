4c50491f2966515359f31bc4944561d67c09f834
Francesco Mari <francescojjmari@gmail.com>
2017-05-20 12:49:22 +0200

    Added edit view, coded edit and destroy actions

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,10 +1,10 @@
 class DocumentsController < ApplicationController
+  before_action :set_document, only: [:show, :edit, :update, :destroy]
   def index
     @documents = Document.order("created_at DESC")
   end
 
   def show
-    @document = Document.find(params[:id])
   end
 
   def new
@@ -24,14 +24,22 @@ class DocumentsController < ApplicationController
   end
 
   def update
+    if @document.update_attributes(document_params)
+      redirect_to @document
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @document.destroy
+    redirect_to 'documents_path'
   end
 
   private
 
-  def find_document
+  def set_document
+    @document = Document.find(params[:id])
   end
   def document_params
     params.require(:document).permit(:title, :content)
