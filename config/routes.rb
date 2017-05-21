451c5067b270070eae5d2a37a0564974cbb9afa4
Francesco Mari <francescojjmari@gmail.com>
2017-05-21 18:17:55 +0200

    Added images and styles for Welcome#index and DocumentsController#index views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'welcome', to: 'welcome#index'
-  root 'welcome#index'
   resources :documents
   authenticated :user do
     root 'documents#index', as: 'authenticated_root'
