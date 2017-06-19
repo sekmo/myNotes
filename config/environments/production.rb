6519e43e5d941a875c99e8df1e8013eb6d1941b4
Francesco Mari <francescojjmari@gmail.com>
2017-06-19 11:55:55 +0200

    Removed relative root directive

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,8 +4,6 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
-  config.action_controller.relative_url_root  = "/var/www/mynotes.sekmo.cc"
-
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
