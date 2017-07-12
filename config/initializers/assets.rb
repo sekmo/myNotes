513e4d9afd74c1374fe7b089073a05dd18b758ad
Francesco Mari <francescojjmari@gmail.com>
2017-07-12 13:29:21 +0200

    Asset pipeline and webfonts fix

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,10 +6,16 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
-Rails.application.config.assets.paths << "#{Rails.root}/vendor/assets/fonts"
+# Rails.application.config.assets.paths << "#{Rails.root}/vendor/assets/fonts"
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
+
+
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( ckeditor/* )
-Rails.application.config.assets.precompile += %w( .ttf .woff .woff2 .otf )
+
+#Rails.application.config.assets.precompile += %w( .ttf .woff .woff2 .otf )
+Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
+#Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf|otf|woff2)\z/
