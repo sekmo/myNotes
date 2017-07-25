6e8386467a96c68116b892fd30c5dfc25a34694d
Francesco Mari <francescojjmari@gmail.com>
2017-07-25 20:31:25 +0200

    Implement search feature, kaminari config

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,7 +2,16 @@ class Note < ApplicationRecord
   belongs_to :user
   validates_presence_of :title, :content, :date
 
-  def self.grouped_by_month(user)
-    Note.where(user_id: user.id).order(date: :DESC).group_by { |note| note.date.beginning_of_month }
+  def self.grouped_by_month
+    order(date: :DESC).group_by { |note| note.date.beginning_of_month }
+  end
+
+  def self.find_by_user(user)
+    where(user: user)
+  end
+
+  def self.find_by_search_term(term)
+    #where(content: term)
+    all
   end
 end
