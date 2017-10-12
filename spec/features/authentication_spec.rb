95ec398b130def2edaff8e12435276a1460887f9
Francesco Mari <francescojjmari@gmail.com>
2017-10-12 16:53:02 +0200

    Fix authentication spec

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -5,12 +5,21 @@ describe "The authentication should work correctly" do
     @user = create(:user, email: "test@example.org", password: "123456")
   end
 
-  it "redirects you to notes#index after a successfully login" do
+  it "welcomes you to after a successfully login" do
     visit root_path
     click_on "Login"
     fill_in "Email", with: "test@example.org"
     fill_in "Password", with: "123456"
     click_on "Log in"
     expect(page).to have_content("Signed in successfully")
+  end
+
+  it "denies access with wrong credentials" do
+    visit root_path
+    click_on "Login"
+    fill_in "Email", with: "test@example.org"
+    fill_in "Password", with: "222222222"
+    click_on "Log in"
+    expect(page).to have_content("Invalid Email or password")
   end
 end
